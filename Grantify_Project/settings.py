f5be3cbdea590d3d86a6f30b7f4bbe7d71416b00
Lidao Betema <rodrigue.betema@gmail.com>
2025-04-11 02:15:17 +0100

    Fix static files configuration

--- Grantify_Project/settings.py
+++ Grantify_Project/settings.py
@@ -186,11 +186,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# WHITENOISE_SKIP_COMPRESS_EXTENSIONS = ['.eot', '.woff', '.woff2', '.ttf', '.svg', '.css', '.js']
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files (for uploaded files)
 MEDIA_URL = '/media/'
