8693aa45591ba4a0a48b70480543740caa111ad1
Lidao Betema <rodrigue.betema@gmail.com>
2025-04-11 01:46:52 +0100

    Update settings.py with Whitenoise exclusions

--- Grantify_Project/settings.py
+++ Grantify_Project/settings.py
@@ -190,6 +190,8 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+WHITENOISE_SKIP_COMPRESS_EXTENSIONS = ['.eot', '.woff', '.woff2', '.ttf', '.svg', '.css', '.js']
+
 # Media files (for uploaded files)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
